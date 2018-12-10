d0cd9dd0f61e64d0b3885fbab7e5a28ffb8587b6
ZaraArican <44377948+ZaraArican@users.noreply.github.com>
2018-12-10 09:43:14 -0500

    Create DoubleLinkedTest.java

---
+++ DoubleLinkedTest.java
@@ -0,0 +1,85 @@
+// ***********************************************************
+// DoubleLinkedTest.java
+//
+// Driver to test DoubleLinked methods.
+// ***********************************************************
+import java.util.Scanner;
+public class DoubleLinkedTest{
+private static Scanner scan;
+private static DoubleLinked list = new DoubleLinked();
+//----------------------------------------------------------------
+// Creates a list, then repeatedly prints the menu and does what
+// the user asks until they quit.
+//----------------------------------------------------------------
+public static void main(String[] args)
+{
+scan = new Scanner(System.in);
+printMenu();
+int choice = scan.nextInt();
+while (choice != 0)
+ {
+dispatch(choice);
+printMenu();
+choice = scan.nextInt();
+ }
+}
+//---------------------------------------
+// Does what the menu item calls for.
+//---------------------------------------
+public static void dispatch(int choice)
+{
+int newVal;
+switch(choice)
+{
+case 0:
+System.out.println("Bye!");
+break;
+case 1: //print
+System.out.println("** List elements **");
+list.print();
+break;
+case 2: //add to front
+System.out.println("Enter integer to add to front");
+newVal = scan.nextInt();
+list.addToFront(newVal);
+break;
+case 3://add to end///
+System.out.println("Enter integer to end of the list");
+newVal = scan.nextInt();
+list.addToEnd(newVal);//list to new valueabl
+break;
+case 4://removes first
+list.removeFirst();
+break;
+case 5://removes the last element of the list. If the list is empty, does nothing.
+list.removeLast();
+break;
+case 6://removes certain number from list
+System.out.println("Enter integer to remove from list");
+newVal = scan.nextInt();
+list.remove(newVal);
+break;
+
+default:
+System.out.println("Sorry, invalid choice");
+}
+}
+//-----------------------------------------
+// Prints the user's choices
+//-----------------------------------------
+public static void printMenu()
+{
+System.out.println("\n Menu ");
+System.out.println(" ====");
+System.out.println("0: Quit");
+System.out.println("1: Print list");
+System.out.println("2: Add an integer to the front of the list");
+System.out.println("3: Add an integer to the end of the list");
+System.out.println("4: Remove the first element of the list");
+System.out.println("5: Remove the last element of the list");//
+System.out.println("6: Remove certain number from list");//
+
+
+System.out.print("\nEnter your choice: ");
+}
+} 
